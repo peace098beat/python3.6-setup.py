a5f7d3e764283a361d6d306e18f96c1aa2bedd2c
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2018-10-14 15:18:10 +0900

    Update setup.py

--- src-paturn/setup.py
+++ src-paturn/setup.py
@@ -1,1 +1,17 @@
-a
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(
+    name='yourapp',
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
