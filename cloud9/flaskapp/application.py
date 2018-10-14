69a642e3f24329b97fb68b5db5356bf51d8097cf
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2018-10-14 15:23:33 +0900

    Update application.py

--- cloud9/flaskapp/application.py
+++ cloud9/flaskapp/application.py
@@ -1,10 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+# coding: utf-8
+import json
+from flask import Flask, Response
+
+app = application = application = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/test")
+def test():
+    return Response(json.dumps({'Output': 'Hello Test'}), mimetype='application/json', status=200)
+
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=true)
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
